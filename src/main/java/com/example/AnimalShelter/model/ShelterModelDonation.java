5c4d728e3bf25aaa84793b4a5336e1d4ce04eb7a
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-08-23 09:41:11 +0200

    model donation

--- src/main/java/com/example/AnimalShelter/model/ShelterModelDonation.java
+++ src/main/java/com/example/AnimalShelter/model/ShelterModelDonation.java
@@ -1,4 +1,29 @@
 package com.example.AnimalShelter.model;
 
-public class ShelterModelDonation {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @Table(name = "donation")
+
+    public class ShelterModelDonation {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long Id;
+
+        @Column(name = "donor_name")
+        private String donor_name;
+
+        @Column(name = "amount")
+        private int amount;
+
+        @Column(name = "donation_date")
+        private String donation_date;
+
 }
