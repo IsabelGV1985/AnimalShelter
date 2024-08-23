afb3ee71d69744eb47680e98763110a758c9308f
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-08-23 09:50:44 +0200

    minor change

--- src/main/java/com/example/AnimalShelter/service/ShelterServiceDonation.java
+++ src/main/java/com/example/AnimalShelter/service/ShelterServiceDonation.java
@@ -1,4 +1,11 @@
 package com.example.AnimalShelter.service;
 
+import com.example.AnimalShelter.model.Donation;
+import com.example.AnimalShelter.repositories.IShelterRepositoryDonation;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ShelterServiceDonation {
+
+    @Autowired
+    IShelterRepositoryDonation iShelterRepositoryDonation;
 }
